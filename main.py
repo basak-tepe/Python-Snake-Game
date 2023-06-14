120de93bb82a3b15ed5d8ddb07bb19ce8f9d1e75
yigitulusoy <123324807+yigitulusoy@users.noreply.github.com>
2023-06-14 22:30:53 +0300

    Added the snake

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+
 import turtle
 
 #main window
 
 mw = turtle.Screen()
 mw.title("Snake Game")
+mw.bgcolor("gray")
+mw.setup(width=800, height=800)
+
+
+#the snake
+snake = turtle.Turtle()
+snake.shape('square')
+snake.color('black')
+snake.speed(0)
+snake.goto(100, 100)
+
+
+
+
+
+
 
 
 mw.mainloop()
