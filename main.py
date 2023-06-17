425d673fdc4e05d6559943cd976a33769a223e49
Başak Tepe <76913595+basak-tepe@users.noreply.github.com>
2023-06-17 17:27:45 +0300

    Added the ability to consume

--- main.py
+++ main.py
@@ -1,99 +1,114 @@
+
 import turtle
 import math
+import random
+import time #to add delays
+delay = 0.2
 
 
-win = True
-time_left = True
+#awaiting updates
+#snake size 
+snake_length = 1
 
+#main window
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("gray")
+wn.setup(width=800, height=800)
+wn.tracer(0)  #No snake is seen at the begining
 
-# Create a screen
-screen = turtle.Screen()
-screen.title("Cake Delivery Game")
-screen.bgcolor("white")
-screen.setup(width=800, height=600)
-screen.tracer(0)
+#the head
+shead = turtle.Turtle()
+shead.shape('square')
+shead.color('black')
+shead.speed(0)
+shead.penup()  #Snake moves without leaving ink behind
+shead.goto(0, 0)
+shead.direction = "stop"
 
-
-
-# End game displays as a list
-# 0 win
-# 1 lose by time
-# 2 lose by error
-end_game_options = ["Congratulations, you delivered the cake on time!",
-            "Failed successfully. You ran out of time and the cake has gone bad!",
-            "Failed successfully. You got crashed by a car!"]
-
-
-#Characters
-characters = ["Burçak","Başak","Elif","Jane", "İrem", "Jack", "Jill", "James", "İlknur", "Jade", "Yigit", "Jasmine", "Jared", "Jocelyn", "Jude", "Jules", "Cengiz", "Juliet", "Julia", "Işıl"]
-
-
-#win / lose display
-def end_game_display(end_game_options):
-    pen = turtle.Turtle()
-    pen.speed(0)
-    pen.color("black")
-    pen.penup()
-    pen.hideturtle()
-    pen.goto(0, 0)
-
-    if win: #win
-        end_game_option = end_game_options[0]
-    elif time_left: #lose by crash
-        end_game_option = end_game_options[2]
-    else: #lose by time
-        end_game_option = end_game_options[1]
-
-    pen.write(end_game_option, align="center", font=("Courier", 24, "normal"))
-
-
-#the sidewalks
-def draw_sidewalks():
-    return 0
-
-
-
-# Create cake of diameter 20
-cake = turtle.Turtle()
-cake.speed(0)
-cake.shape("circle")
-cake.color("pink")
-cake.penup()
-cake.goto(0, 0)
-cake.direction = "stop"
-screen.update()
-
-#moving the cake with arrow keys
-# Moving the cake
+#fns
 def move_up():
-    y = cake.ycor()
-    y += 20
-    cake.sety(y)
-    screen.update()
+    shead.direction = "up"
 
 def move_down():
-    y = cake.ycor()
-    y -= 20
-    cake.sety(y)
-    screen.update()
+    shead.direction = "down"
 
 def move_left():
-    x = cake.xcor()
-    x -= 20
-    cake.setx(x)
-    screen.update()
+    shead.direction = "left"
 
 def move_right():
-    x = cake.xcor()
-    x += 20
-    cake.setx(x)
-    screen.update()
+    shead.direction = "right"
+
+def move():
+    consume_food()
+    if shead.direction == "up":
+        y = shead.ycor()
+        shead.sety(y + 20)
+
+    if shead.direction == "down":
+        y = shead.ycor()
+        shead.sety(y - 20)
+
+    if shead.direction == "left":
+        x = shead.xcor()
+        shead.setx(x - 20)
+
+    if shead.direction == "right":
+        x = shead.xcor()
+        shead.setx(x + 20)
+
+#keyboard bindings
+wn.listen()
+wn.onkeypress(move_up, "w")
+wn.onkeypress(move_down, "s")
+wn.onkeypress(move_left, "a")
+wn.onkeypress(move_right, "d")
+
+global food
+global food_exists #food presence
+food_exists = False
+
+#generate food on the screen
+#random coordinates each time
+def generate_food():
+    global food_exists
+    global food
+    if food_exists == False:
+        x = random.randint(-400, 400)
+        y = random.randint(-400, 400)
+        food = turtle.Turtle()
+        food.shape('circle')
+        food.color('red')
+        food.speed(0)
+        food.penup()
+        food.goto(x, y)
+        food_exists = True
+        return food
+    else:
+        return None
+
+def consume_food():
+    global food_exists
+    global food
+    global snake_length
+    if shead.distance(food) < 20:
+        #we should have used clear
+        food.hideturtle()
+        snake_length += 1
+        food_exists = False
+        return True
+    else:
+        return False
+
+#main game
+while True:
+    wn.update()
+    generate_food()
+    move()
+    time.sleep(delay)
 
 
-# Bind arrow keys to movement functions
-screen.onkey(move_up, "Up")
-screen.onkey(move_down, "Down")
-screen.onkey(move_left, "Left")
-screen.onkey(move_right, "Right")
-screen.listen()
-turtle.done()
+
+
+
+wn.mainloop()
