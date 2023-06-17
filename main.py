a69344feefb6f0470720ccd4f4c70513c3ada70e
Başak Tepe <76913595+basak-tepe@users.noreply.github.com>
2023-06-17 17:17:47 +0300

    Add random food spawn

--- main.py
+++ main.py
@@ -1,73 +1,99 @@
-
 import turtle
-import time #to add delays
-delay = 0.2
+import math
 
-#main window
 
-wn = turtle.Screen()
-wn.title("Snake Game")
-wn.bgcolor("gray")
-wn.setup(width=800, height=800)
-wn.tracer(0)  #No snake is seen at the begining
+win = True
+time_left = True
 
-#the head
-shead = turtle.Turtle()
-shead.shape('square')
-shead.color('black')
-shead.speed(0)
-shead.penup()  #Snake moves without leaving ink behind
-shead.goto(0, 0)
-shead.direction = "stop"
 
-#fns
+# Create a screen
+screen = turtle.Screen()
+screen.title("Cake Delivery Game")
+screen.bgcolor("white")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+
+
+# End game displays as a list
+# 0 win
+# 1 lose by time
+# 2 lose by error
+end_game_options = ["Congratulations, you delivered the cake on time!",
+            "Failed successfully. You ran out of time and the cake has gone bad!",
+            "Failed successfully. You got crashed by a car!"]
+
+
+#Characters
+characters = ["Burçak","Başak","Elif","Jane", "İrem", "Jack", "Jill", "James", "İlknur", "Jade", "Yigit", "Jasmine", "Jared", "Jocelyn", "Jude", "Jules", "Cengiz", "Juliet", "Julia", "Işıl"]
+
+
+#win / lose display
+def end_game_display(end_game_options):
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color("black")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0, 0)
+
+    if win: #win
+        end_game_option = end_game_options[0]
+    elif time_left: #lose by crash
+        end_game_option = end_game_options[2]
+    else: #lose by time
+        end_game_option = end_game_options[1]
+
+    pen.write(end_game_option, align="center", font=("Courier", 24, "normal"))
+
+
+#the sidewalks
+def draw_sidewalks():
+    return 0
+
+
+
+# Create cake of diameter 20
+cake = turtle.Turtle()
+cake.speed(0)
+cake.shape("circle")
+cake.color("pink")
+cake.penup()
+cake.goto(0, 0)
+cake.direction = "stop"
+screen.update()
+
+#moving the cake with arrow keys
+# Moving the cake
 def move_up():
-    shead.direction = "up"
+    y = cake.ycor()
+    y += 20
+    cake.sety(y)
+    screen.update()
 
 def move_down():
-    shead.direction = "down"
+    y = cake.ycor()
+    y -= 20
+    cake.sety(y)
+    screen.update()
 
 def move_left():
-    shead.direction = "left"
+    x = cake.xcor()
+    x -= 20
+    cake.setx(x)
+    screen.update()
 
 def move_right():
-    shead.direction = "right"
-
-def move():
-    if shead.direction == "up":
-        y = shead.ycor()
-        shead.sety(y + 25)
-
-    if shead.direction == "down":
-        y = shead.ycor()
-        shead.sety(y - 25)
-
-    if shead.direction == "left":
-        x = shead.xcor()
-        shead.setx(x - 25)
-
-    if shead.direction == "right":
-        x = shead.xcor()
-        shead.setx(x + 25)
-
-#keyboard bindings
-wn.listen()
-wn.onkeypress(move_up, "w")
-wn.onkeypress(move_down, "s")
-wn.onkeypress(move_left, "a")
-wn.onkeypress(move_right, "d")
+    x = cake.xcor()
+    x += 20
+    cake.setx(x)
+    screen.update()
 
 
-#main game
-while True:
-    wn.update()
-
-    move()
-
-    time.sleep(delay)
-
-
-
-
-
-wn.mainloop()
+# Bind arrow keys to movement functions
+screen.onkey(move_up, "Up")
+screen.onkey(move_down, "Down")
+screen.onkey(move_left, "Left")
+screen.onkey(move_right, "Right")
+screen.listen()
+turtle.done()
